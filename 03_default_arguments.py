65850312beb242c9e5042c7c0cce8909ec2830f1
Pranshu Gajera <pranshugajra77@gmail.com>
2025-05-16 18:34:45 +0530

    default arguments

---
+++ 03_default_arguments.py
@@ -0,0 +1,8 @@
+# +++++++++++++++ function's default arguments ++++++++++++++++
+
+def goodDay(name,ending = "Thank You"):
+    print(f"goodDay, {name}")
+    print(ending)
+    
+goodDay("Pranshu","Thank you")
+goodDay("Rohan")
